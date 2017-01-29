e5f7fce26c681bf841ab6400ac9e770694024680
Manuel Vilachán <mvilachan@gmail.com>
2017-01-30 00:33:01 +0100

    Add extension method & ctor with simple data types to ease configuration through external config files

---
+++ test/Serilog.Sinks.RavenDB.Tests/DocumentStoreTestListener.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using Raven.Client.Listeners;
+using Raven.Json.Linq;
+using LogEvent = Serilog.Sinks.RavenDB.Data.LogEvent;
+
+namespace Serilog.Sinks.RavenDB.Tests
+{
+    public class DocumentStoreTestListener : IDocumentStoreListener
+    {
+        public Dictionary<string, LogEvent> Events { get; set; } = new Dictionary<string, LogEvent>();
+
+        public bool BeforeStore(string key, object entityInstance, RavenJObject metadata, RavenJObject original)
+        {
+            return true;
+        }
+
+        public void AfterStore(string key, object entityInstance, RavenJObject metadata)
+        {
+            Events.Add(key, (LogEvent) entityInstance);
+        }
+    }
+}
